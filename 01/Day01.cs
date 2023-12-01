f19d7d0c1f08a5dafa81b06ecfee1ab2bba7d772
Flavia Brogle <flavia.brogle@fhnw.ch>
2023-12-01 12:58:55 +0100

    Day01 First part solved

--- 01/Day01.cs
+++ 01/Day01.cs
@@ -3,7 +3,29 @@ namespace _01;
 public class Day01 {
 
     public static void Main(string[] args) {
-        var input = File.ReadAllLines("input.txt").Select(int.Parse).ToList();
+        var allLines = File.ReadAllLines("input.txt").ToList();
+        // using var outp = new StreamWriter("output.txt");
+        // outp.Write("solution");
+
+        var total = 0;
+
+        foreach (var line in allLines) {
+            var a = -1;
+            var b = -1;
+            for (int i = 0; i < line.Length; i++) {
+                var c = line[i];
+                if (c is >= '0' and <= '9') {
+                    var n = int.Parse($"{c}");
+                    if (a == -1) a = n;
+                    b = n;
+                }
+            }
+
+            total += 10 * a + b;
+
+        }
+
+        Console.WriteLine($"calibration value: {total}");
         
     }
     

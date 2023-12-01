62d16e05e8df1b3cd255041c0c0655879d25704c
Flavia Brogle <flavia.brogle@fhnw.ch>
2023-12-01 12:47:17 +0100

    Start of Advent of Code project

---
+++ 01/Day01.cs
@@ -0,0 +1,10 @@
+namespace _01; 
+
+public class Day01 {
+
+    public static void Main(string[] args) {
+        var input = File.ReadAllLines("input.txt").Select(int.Parse).ToList();
+        
+    }
+    
+}

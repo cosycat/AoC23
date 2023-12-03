96575905fd023dd81d87c79d2c766e7cdc8c9ee4
Flavia Brogle <flavia.brogle@fhnw.ch>
2023-12-03 15:17:16 +0100

    Add Template to create new AoC projects

--- AoCTemplate/Dayxx.cs
+++ AoCTemplate/Dayxx.cs
@@ -1,50 +1,51 @@
 using System.Diagnostics;
 
-namespace Templates; 
+namespace dxxyCurrYear; 
 
 public static class Dayxx {
-    private static string InputFileName => "inputDayxx.txt";
-    private static string TestFileName => "testInputDayxx.txt";
-    public static /*resultType*/int ExpectedResultTest1 => 0; // TODO replace
-    public static /*resultType*/int ExpectedResultTest2 => 0; // TODO replace
-    
+    private const /*resultType*/int ExpectedResultTest1 = 0; // TODO replace
+    private const /*resultType*/int ExpectedResultTest2 = 0; // TODO replace
+    private const string InputFileName = "inputDayxx.txt";
+    private const string TestFileName = "testInputDayxx.txt";
     private static bool Test2Started => ExpectedResultTest2 != 0;
-    
 
     public static void Main(string[] args) {
-        // TEST
+        TestRun();
+
+        Solve(InputFileName, out var result1, out var result2);
+        Console.WriteLine($"Result 1: {result1}");
+        Console.WriteLine($"Result 2: {result2}");
+    }
+    
+    [Conditional("DEBUG")]
+    private static void TestRun() {
         Solve(TestFileName, out var resultTest1, out var resultTest2);
-        const string success = "✅"; const string fail = "❌";
-        Console.WriteLine($"Test result 1: {(resultTest1 == ExpectedResultTest1 ? success : fail)} (result: {resultTest1}, expected: {ExpectedResultTest1})");
+        const string success = "✅";
+        const string fail = "❌";
+        Console.WriteLine(
+            $"Test result 1: {(resultTest1 == ExpectedResultTest1 ? success : fail)} (result: {resultTest1}, expected: {ExpectedResultTest1})");
         if (Test2Started)
-            Console.WriteLine($"Test result 2: {(resultTest2 == ExpectedResultTest2 ? success : fail)} (result: {resultTest2}, expected: {ExpectedResultTest2})");
+            Console.WriteLine(
+                $"Test result 2: {(resultTest2 == ExpectedResultTest2 ? success : fail)} (result: {resultTest2}, expected: {ExpectedResultTest2})");
         Console.WriteLine();
 
         Debug.Assert(ExpectedResultTest1 != 0, "No expected result for test 1 set!");
         Debug.Assert(ExpectedResultTest1 == resultTest1, "Test 1 failed!");
         Debug.Assert(!Test2Started || ExpectedResultTest2 == resultTest2, "Test 2 failed!");
-    
-
-        // ACTUAL INPUT
-        Solve(InputFileName, out var result1, out var result2);
-        Console.WriteLine($"Result 1: {result1}");
-        Console.WriteLine($"Result 2: {result2}");
     }
 
-    public static void Solve(string inputFileName, out /*resultType*/int result1, out /*resultType*/int result2) {
+    private static void Solve(string inputFileName, out /*resultType*/int result1, out /*resultType*/int result2) {
         result1 = 0; 
         result2 = 0;
-
-#if(LineByLineArray)
-        var allLines = File.ReadAllLines(inputFileName).ToArray(); // .ToList();
-        var width = allLines[0].Length;
-        var height = allLines.Length; // .Count;
-#elif(LineByLineList)
+        
+#if(LineByLineList)
         var allLines = File.ReadAllLines(inputFileName).ToList(); // .ToArray();
+        Debug.Assert(allLines.Length > 0, $"Input file {inputFileName} is empty!");
         var width = allLines[0].Length;
         var height = allLines.Count; // .Length;
-#else
+#else // LineByLineArray (default)
         var allLines = File.ReadAllLines(inputFileName).ToArray(); // .ToList();
+        Debug.Assert(allLines.Length > 0, $"Input file {inputFileName} is empty!");
         var width = allLines[0].Length;
         var height = allLines.Length; // .Count;
 #endif
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # AoC23
 
 Advent of Code challenge 2023
+
+## Template Usage
+
+### Installing the Template
+
+```
+cd <path to AoCTemplate>
+dotnet new install AoCTemplate --force
+```
+
+### Create a New Project
+
+Example to create a new project for day 1:
+
+```
+day=01 && dotnet new AoC -o $day -D $day -R int && dotnet sln add $day
+```
+
+For a different year than 2023, use the `-Y` option, or edit the default value for the Year parameter in the template.json file.
+
+Use `dotnet new AoC --help` for more information.
